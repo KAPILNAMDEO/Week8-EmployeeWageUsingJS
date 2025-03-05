2171b305ab4823559c2313458168c090e424bfcc
kapil namdeo <kapilnamdeo2003@gmail.com>
2025-03-05 11:44:44 +0530

    [Kapil_Namdeo]Add. Ability to Calculate Daily Employee Wage based on work type

--- EmployeeWageUsingJS/src/index.js
+++ EmployeeWageUsingJS/src/index.js
@@ -1,4 +1,5 @@
-const EmployeeWage = require("./EmployeeWage"); // Import the class
+const EmployeeWage = require("./EmployeeWage");
 
 const employee = new EmployeeWage("Kapil Namdeo");
-console.log(employee.getAttendance()); // Check employee attendance
+console.log(employee.getAttendance());
+console.log(employee.getDailyWage());
