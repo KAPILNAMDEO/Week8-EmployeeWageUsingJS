2e7d746bc92babad411bf02c759c5bbdea1897a1
kapil namdeo <kapilnamdeo2003@gmail.com>
2025-03-05 11:36:35 +0530

    [Kapil_namdeo]Add.UC01 check employee presence uc

---
+++ EmployeeWageUsingJS/src/index.js
@@ -0,0 +1,4 @@
+const EmployeeWage = require("./EmployeeWage"); // Import the class
+
+const employee = new EmployeeWage("Kapil Namdeo");
+console.log(employee.getAttendance()); // Check employee attendance
